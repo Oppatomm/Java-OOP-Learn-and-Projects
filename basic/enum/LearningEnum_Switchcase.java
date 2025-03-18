4c477c9646c29775c49f129c1676cf0411de2080
Sumate Boonkird <sumateboonkird5555github@gmail.com>
2025-03-18 17:43:25 +0700

    Add File : LearningEnum_Switchcase.java

---
+++ basic/enum/LearningEnum_Switchcase.java
@@ -0,0 +1,21 @@
+enum Day {
+    MONDAY , 
+    TUESDAY , 
+    WEDNESDAY ,
+    THURSDAY ,
+    FRIDAY ,
+    SATURDAY ,
+    SUNDAY
+}
+
+public class LearningEnum_Switchcase {
+    public static void main (String [] args) {
+        Day today = Day.MONDAY;
+
+        switch (today) {
+            case MONDAY , TUESDAY , WEDNESDAY , THURSDAY , FRIDAY -> System.out.println("Weekday");
+            case SATURDAY , SUNDAY -> System.out.println("Weekend");
+            default -> System.out.println("Invalid day");
+        }
+    }
+}
